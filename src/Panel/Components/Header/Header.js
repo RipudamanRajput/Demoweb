4a59e6131886da488bde02ed45272b3085d26639
Ripudman-cedcoss <ripudamanrajput@cedcommerce.com>
2022-10-04 19:17:28 +0530

    responsive design

--- src/Panel/Components/Header/Header.js
+++ src/Panel/Components/Header/Header.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 import { Menu } from "react-feather";
 import Topbar from "./Topbar/Topbar";
-import './Topbar/style.css'
+import './Topbar/style.css';
+import logo from '../../../Assets/img/logo/logo-removebg-preview.png';
+
 function Header() {
     const [show, setshow] = useState(false)
     return(
         <>
+        <span className="header-logo">
+            <span className="logo-container">
+                <img src={logo} width={60}/>
+            </span>
+        </span>
        <span 
          className="top-bar--toggler"
          onClick={()=>setshow(!show)}
