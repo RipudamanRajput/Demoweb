8f554d9cfab4aa6353eb71c37630b2635a27097e
ripudaman-cedcoss <ripudamanrajput@cedcommerce.com>
2022-10-05 13:58:03 +0530

    design changes on about us page

--- src/Panel/Components/Component.js
+++ src/Panel/Components/Component.js
@@ -2,19 +2,19 @@ import React from "react";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Bodylayout from "./BodySection/Bodysection";
-import { Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Aboutus from "./BodySection/AboutUs/Aboutus";
 
 function Components() {
     return (
         <>
             <Header />
-            <Routes>
-                <Route path="/Demoweb" element={<Bodylayout />} />
-                <Route path="Demoweb/aboutus" element={<Aboutus />} />
-                <Route path="Demoweb/Policy" element={<h1>Policy</h1>} />
-                <Route path="Demoweb/Contact" element={<h1>Contact Us</h1>} />
-            </Routes>
+                <Routes>
+                    <Route path="/Demoweb" element={<Bodylayout />} />
+                    <Route path="Demoweb/aboutus" element={<Aboutus />} />
+                    <Route path="Demoweb/Policy" element={<h1>Policy</h1>} />
+                    <Route path="Demoweb/Contact" element={<h1>Contact Us</h1>} />
+                </Routes>
             <Footer />
         </>
     )
